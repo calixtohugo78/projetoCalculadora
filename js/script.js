8dfee2da27cd5bcd4fca9c279d2a00296e1c6c1a
Hugo Vasconcellos <vasconcelloshugo09@gmail.com>
2022-05-10 21:59:35 -0300

    implant calc function

--- js/script.js
+++ js/script.js
@@ -3,14 +3,41 @@ let calcScreen = document.querySelector(".calcScreen")
 
 calcBody.addEventListener('click', (e) => {
 
-    if(e.target.id) {
+    if (e.target.id) {
 
-        if(e.target.innerText == "AC"){
-            calcScreen.innerText = "0"
-        } else {
-            calcScreen.innerText += e.target.innerText    
+        let btn = e.target.innerText;
+
+        switch (btn) {
+
+            case 'AC':
+                calcScreen.innerText = "";
+                break
+
+            case '=':
+                printResult(calcScreen.innerText);
+            break
+
+            default:
+                calcScreen.innerText += e.target.innerText
+
         }
 
     }
 
 })
+
+const printResult = (stringResult) => {
+
+    let issetDiv = stringResult.indexOf("/");
+    let issetMult = stringResult.indexOf("x");
+    let issetSub = stringResult.indexOf("-");
+    let issetAdd = stringResult.indexOf("+");
+
+    if (issetDiv != -1) {
+        let splitResult = stringResult.split("*");
+
+        console.log(splitResult)
+    }
+
+}
+
